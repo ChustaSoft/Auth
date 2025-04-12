841a5d9290d0c644ce34296485f8696fa6993751
xaberue <xavier.aberue@outlook.es>
2025-04-12 21:14:14 +0200

    Added http file for Basic sample

--- Examples/Basic/Program.cs
+++ Examples/Basic/Program.cs
@@ -2,16 +2,14 @@ using ChustaSoft.Tools.Authorization.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.ConfigureBasicAuthentication((username, password) => { return username == "test" && password == "pass"; });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
