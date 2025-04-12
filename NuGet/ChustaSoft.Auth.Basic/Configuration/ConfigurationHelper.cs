abcc2fa478b85e6da849796b812adb59bd90ea39
xaberue <xavier.aberue@outlook.es>
2025-04-12 20:42:41 +0200

    Few enhancements from current C# / dotnet versions

--- NuGet/ChustaSoft.Auth.Basic/Configuration/ConfigurationHelper.cs
+++ NuGet/ChustaSoft.Auth.Basic/Configuration/ConfigurationHelper.cs
@@ -3,16 +3,16 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 
-namespace ChustaSoft.Tools.Authorization.Configuration
-{
-    public static class ConfigurationHelper
-    {
-        public static void ConfigureBasicAuthentication(this IServiceCollection services, Func<string, string, bool> userCredentialsProvider, string schemaName = "BasicAuthentication")
-        {
-            services.AddTransient<Func<string, string, bool>>(x => userCredentialsProvider);
+namespace ChustaSoft.Tools.Authorization.Configuration;
 
-            services.AddAuthentication(schemaName)
-                .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>(schemaName, null);
-        }
+
+public static class ConfigurationHelper
+{
+    public static void ConfigureBasicAuthentication(this IServiceCollection services, Func<string, string, bool> userCredentialsProvider, string schemaName = "BasicAuthentication")
+    {
+        services.AddTransient<Func<string, string, bool>>(x => userCredentialsProvider);
+
+        services.AddAuthentication(schemaName)
+            .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>(schemaName, null);
     }
 }
